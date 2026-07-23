82b211f51c2a45fc970bfc6ea238234271048f0f
KIRI <kehhu.tm@gmail.com>
2026-07-23 20:21:42 +1100

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.NOIRWAVE_CONFIG = Object.freeze({
   API_BASE: "https://noirwave-api.wxzg4bgc6n.workers.dev",
-  GOOGLE_CLIENT_ID: "311673024413-a4i47b4ofel7vi5lju6703jp1ccf6124.apps.googleusercontent.com"
+  GOOGLE_CLIENT_ID: "311673024413-a4i47b4ofel7vi5lju6703ip1ccf6124.apps.googleusercontent.com"
 });
